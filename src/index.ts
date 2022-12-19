30b0ff16dff3604a7334398ca1e12bdce01d2db3
Flamur Mavraj <flamur.mavraj@ornio.no>
2022-12-19 20:57:15 +0100

    WIP Install husky and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 const port = 6001;
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({data: "Hello from us! ..."});
+  res.status(200).send({ data: 'Hello from us! ...' });
 });
 
 app.listen(port, () => console.log(`Server is listing on port:${port}`));
