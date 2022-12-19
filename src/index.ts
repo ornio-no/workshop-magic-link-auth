72d0d46726edb6c6d01c8f6d7933e3eb28499fcb
Flamur Mavraj <flamur.mavraj@ornio.no>
2022-12-19 20:33:49 +0100

    Installed express, ts-node and nodemon

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+
+const app: Application = express();
+const port = 6001;
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({data: "Hello from us! ..."});
+});
+
+app.listen(port, () => console.log(`Server is listing on port:${port}`));
